d86a51384aced7c15d65c59ea75575dbbb2e8af1
elifcobann <95699826+elifcobann@users.noreply.github.com>
2024-05-20 10:38:17 +0300

    Proje dosyası ekle.

---
+++ ToDoApp/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ToDoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TitleTextBox = new System.Windows.Forms.TextBox();
+            this.Title = new System.Windows.Forms.Label();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.deletebutton = new System.Windows.Forms.Button();
+            this.newbutton = new System.Windows.Forms.Button();
+            this.todolistview = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.todolistview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TO DO LİST ";
+            // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TitleTextBox.Location = new System.Drawing.Point(12, 137);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.Size = new System.Drawing.Size(777, 34);
+            this.TitleTextBox.TabIndex = 1;
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Title.Location = new System.Drawing.Point(12, 112);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(50, 22);
+            this.Title.TabIndex = 2;
+            this.Title.Text = "Title";
+            // 
+            // savebutton
+            // 
+            this.savebutton.BackColor = System.Drawing.Color.Honeydew;
+            this.savebutton.Location = new System.Drawing.Point(564, 266);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(190, 32);
+            this.savebutton.TabIndex = 5;
+            this.savebutton.Text = "Save";
+            this.savebutton.UseVisualStyleBackColor = false;
+            this.savebutton.Click += new System.EventHandler(this.savebutton_Click);
+            // 
+            // deletebutton
+            // 
+            this.deletebutton.BackColor = System.Drawing.Color.Honeydew;
+            this.deletebutton.Location = new System.Drawing.Point(304, 266);
+            this.deletebutton.Name = "deletebutton";
+            this.deletebutton.Size = new System.Drawing.Size(190, 32);
+            this.deletebutton.TabIndex = 6;
+            this.deletebutton.Text = "Delete";
+            this.deletebutton.UseVisualStyleBackColor = false;
+            this.deletebutton.Click += new System.EventHandler(this.deletebutton_Click);
+            // 
+            // newbutton
+            // 
+            this.newbutton.BackColor = System.Drawing.Color.Honeydew;
+            this.newbutton.Location = new System.Drawing.Point(32, 266);
+            this.newbutton.Name = "newbutton";
+            this.newbutton.Size = new System.Drawing.Size(190, 32);
+            this.newbutton.TabIndex = 8;
+            this.newbutton.Text = "New";
+            this.newbutton.UseVisualStyleBackColor = false;
+            this.newbutton.Click += new System.EventHandler(this.newbutton_Click);
+            // 
+            // todolistview
+            // 
+            this.todolistview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.todolistview.BackgroundColor = System.Drawing.Color.GhostWhite;
+            this.todolistview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.todolistview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column1,
+            this.Column2});
+            this.todolistview.GridColor = System.Drawing.Color.Black;
+            this.todolistview.Location = new System.Drawing.Point(12, 311);
+            this.todolistview.Name = "todolistview";
+            this.todolistview.RowHeadersWidth = 51;
+            this.todolistview.RowTemplate.Height = 24;
+            this.todolistview.Size = new System.Drawing.Size(776, 392);
+            this.todolistview.TabIndex = 9;
+            this.todolistview.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.todolistview_CellClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(32, 211);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(154, 33);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Completed";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Id";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Görev";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Durum";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(798, 711);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.todolistview);
+            this.Controls.Add(this.newbutton);
+            this.Controls.Add(this.deletebutton);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.TitleTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "To Do list";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.todolistview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TitleTextBox;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button savebutton;
+        private System.Windows.Forms.Button deletebutton;
+        private System.Windows.Forms.Button newbutton;
+        private System.Windows.Forms.DataGridView todolistview;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ ToDoApp/Form1.cs
@@ -0,0 +1,173 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int rowIndex = -1;
+        public int rowId = -1;
+        public int colIndex = -1;  
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getAllToDoList();
+        }
+
+        private void newbutton_Click(object sender, EventArgs e)
+        {
+            reset();
+            rowId = -1;
+            rowIndex = -1;
+        }
+
+        private async void deletebutton_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var resp = await client.DeleteAsync("https://localhost:7281/api/todoitems/" + rowId);
+            rowIndex = -1;
+            rowId = -1;
+            getAllToDoList();
+            reset();
+
+
+
+
+        }
+
+        private async void savebutton_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();  // get post gibi istekler için
+            
+            var obj = new
+            {
+                //rowIndex,
+                id = rowId,
+                name = TitleTextBox.Text,
+                isComplete = checkBox1.Checked,
+            };
+
+            
+            if (rowId <= -1)
+            {
+                
+
+                var resp = await client.PostAsync("https://localhost:7281/api/todoitems",
+                    new StringContent(JsonConvert.SerializeObject(obj),
+                    Encoding.UTF8, "application/json")).Result.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                var resp = await client.PutAsync("https://localhost:7281/api/todoitems/" + rowId, 
+                    new StringContent(JsonConvert.SerializeObject(obj),
+                    Encoding.UTF8, "application/json")).Result.Content.ReadAsStringAsync();
+
+                Console.WriteLine(resp);
+            }
+            
+             getAllToDoList();
+            reset();
+            rowIndex = -1;
+            rowId = -1;
+
+
+        }
+
+        public async void getAllToDoList()
+        {   
+
+            todolistview.Rows.Clear(); // tüm verileri tekrar kaydetmemek için listeyi temizler.
+            var client = new HttpClient(); 
+    
+            var resp = await client.GetAsync("https://localhost:7281/api/todoitems").Result.Content.ReadAsStringAsync();
+            if (resp != null)
+            {
+                var respList = JsonConvert.DeserializeObject<listModel[]>(resp);
+                foreach (var item in respList)
+                {
+                    var index = todolistview.Rows.Add();
+                    todolistview.Rows[index].Cells["Column1"].Value = item.name;
+                    todolistview.Rows[index].Cells["Column2"].Value = item.isComplete == true ? "\u2714" : "X";
+                    todolistview.Rows[index].Cells["Column3"].Value = item.id;
+
+                    // respe gelenleri listmodel şeklinde respliste liste şeklinde çeviriyor
+
+
+                    
+                        // Satırın "isComplete" özelliğini kontrol etmek için bir if bloğu
+                    if (item.isComplete == true)
+                    {
+                    // "isComplete" özelliği true olduğunda, ilgili satırın arka plan rengini yeşil yap
+                        todolistview.Rows[index].DefaultCellStyle.BackColor = Color.LightGreen;
+                    }
+                    else
+                    {
+                        todolistview.Rows[index].DefaultCellStyle.BackColor = Color.Red;
+                    }
+                  
+
+                }
+              }
+
+                                                    
+                 
+        }
+        private void todolistview_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow clickedRow = todolistview.Rows[e.RowIndex];
+            rowIndex = e.RowIndex;
+            TitleTextBox.Text = (string)todolistview.Rows[e.RowIndex].Cells["Column1"].Value;
+            checkBox1.Checked = todolistview.Rows[e.RowIndex].Cells["Column2"].Value == "\u2714" ? true : false;       
+            rowId = (int)todolistview.Rows[e.RowIndex].Cells["Column3"].Value;
+
+
+
+            /*  todolistview.Rows.Add(item.name);
+
+              if (item.isComplete == true)
+              {
+                 todolistview.Rows[index].Cells["Column2"].Value = "yapıldı";
+                  Console.WriteLine("yapıldı");
+              }
+              else
+              {
+                  todolistview.Rows[index].Cells["Column2"].Value = "yapılmadı";
+              }*/
+
+
+            //todolistview.Rows.RemoveAt(e.RowIndex);
+        }
+
+
+        public void reset()
+        {
+            TitleTextBox.Text = "";
+            checkBox1.Checked = false;
+        }
+    }
+    public class listModel
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
+
+
